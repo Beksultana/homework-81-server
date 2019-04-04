ba21943a1d19692e7e71a3c7273e36f299fe9101
sultan <aidaraliuulu@gmail.com>
2019-04-04 13:44:18 +0600

    Написал server.sj

--- server.js
+++ server.js
@@ -1,3 +1,13 @@
 const express = require('express');
 const Mongoose = require('mongoose');
-const cors = require('cors')
+const cors = require('cors');
+const app = express();
+
+const port = 6500;
+
+app.use(express.json());
+app.use(cors());
+
+app.listen(port, () => {
+    console.log(`Server started on ${port} port!`);
+});
